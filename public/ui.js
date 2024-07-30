fdaebe657c694a563d4c44c4b80346b4739c748f
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-30 10:34:06 +0530

    feat: prep game screens

---
+++ public/ui.js
@@ -0,0 +1,33 @@
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+
+const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
+const spawnModal = new bootstrap.Modal(document.getElementById("spawnModal"));
+
+const canvas = document.getElementById("the-canvas");
+const context = canvas.getContext("2d");
+
+canvas.height = wHeight;
+canvas.width = wWidth;
+const player = {};
+
+window.addEventListener("load", () => {
+  loginModal.show();
+});
+
+document.querySelector(".name-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  player.name = document.getElementById("name-input").value;
+  document.querySelector(".player-name").textContent = player.name;
+  loginModal.hide();
+  spawnModal.show();
+});
+
+document.querySelector(".start-game").addEventListener("click", () => {
+  spawnModal.hide();
+  const elements = Array.from(document.querySelectorAll(".hiddenOnStart"));
+  elements.forEach((element) => {
+    element.removeAttribute("hidden");
+  });
+  init();
+});
