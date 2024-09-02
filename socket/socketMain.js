e72d1ebd52f952c7e22d5c3406c5c54a8f45697c
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-09-02 07:33:02 +0530

    feat(server): init player classes

--- socket/socketMain.js
+++ socket/socketMain.js
@@ -1,17 +1,32 @@
 const { io, app } = require("../server");
-
+const Player = require("./classes/Player");
+const PlayerConfig = require("./classes/PlayerConfig");
+const PlayerData = require("./classes/PlayerData");
 const Orb = require("./classes/Orb");
 
 const orbs = [];
+const settings = {
+  defaultNumberOfOrbs: 500,
+  defaultSpeed: 6,
+  defaultSize: 6,
+  defaultZoom: 1.5,
+  worldWidth: 500,
+  worldHeight: 500,
+  defaultGenericOrbSize: 5,
+};
 
 initGame();
 
 io.on("connect", (socket) => {
+  const playerName = "Test";
+  const playerConfig = new PlayerConfig(settings);
+  const playerData = new PlayerData(playerName, settings);
+  const player = new Player(socket.id, playerConfig, playerData);
   socket.emit("init", { orbs });
 });
 
 function initGame() {
-  for (let i = 0; i < 500; i++) {
-    orbs.push(new Orb());
+  for (let i = 0; i < settings.defaultNumberOfOrbs; i++) {
+    orbs.push(new Orb(settings));
   }
 }
