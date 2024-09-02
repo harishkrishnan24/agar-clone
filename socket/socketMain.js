3c6d4f7fd6d2a6f2e5714b31b96f8ddb48ae7391
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-09-02 07:48:41 +0530

    chore: refactor socket initialization logic

--- socket/socketMain.js
+++ socket/socketMain.js
@@ -14,15 +14,19 @@ const settings = {
   worldHeight: 500,
   defaultGenericOrbSize: 5,
 };
+const players = [];
 
 initGame();
 
 io.on("connect", (socket) => {
-  const playerName = "Test";
-  const playerConfig = new PlayerConfig(settings);
-  const playerData = new PlayerData(playerName, settings);
-  const player = new Player(socket.id, playerConfig, playerData);
-  socket.emit("init", { orbs });
+  socket.on("init", (playerObj, ackCallback) => {
+    const playerName = playerObj.playerName;
+    const playerConfig = new PlayerConfig(settings);
+    const playerData = new PlayerData(playerName, settings);
+    const player = new Player(socket.id, playerConfig, playerData);
+    players.push(player);
+    ackCallback(orbs);
+  });
 });
 
 function initGame() {
